30a225c62443b0d84be6cb8342908c46528feec3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-02 13:10:13 -0400

    Add second eks module for comparison

--- runtime_eks/region/main.tf
+++ runtime_eks/region/main.tf
@@ -9,3 +9,34 @@ module "eks" {
   vpc_id             = module.network.vpc_id
   private_subnet_ids = module.network.vpc_private_subnet_ids
 }
+
+module "eks_v2" {
+  count  = var.cluster_name != null ? 1 : 0
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.16.0"
+
+  cluster_name    = "${var.cluster_name}-v2"
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  vpc_id     = module.network.vpc_id
+  subnet_ids = module.network.vpc_private_subnet_ids
+
+  eks_managed_node_group_defaults = {
+    create_iam_role = true
+    ami_type        = "AL2_x86_64"
+    disk_size       = 100
+  }
+
+  eks_managed_node_groups = {
+    hashicups = {
+      use_custom_launch_template = false
+
+      min_size     = 1
+      max_size     = 5
+      desired_size = 3
+    }
+  }
+}
+
