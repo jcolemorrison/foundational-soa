d82300f246cfba26b7a4c421f0b6d4e2ae0d2109
J Cole Morrison <me@jcolemorrison.com>
2023-11-01 23:52:49 -0700

    add network to us-west-2 for the ec2 runtime

--- runtime_ec2/main.tf
+++ runtime_ec2/main.tf
@@ -1,5 +1,6 @@
 locals {
   us_east_1 = "us-east-1"
+  us_west_2 = "us-west-2"
   transit_gateway_ids = data.terraform_remote_state.shared_services.outputs.transit_gateway_ids
   shared_services_cidr_blocks = data.terraform_remote_state.shared_services.outputs.shared_services_cidr_blocks
   hcp_hvn_cidr_blocks = data.terraform_remote_state.shared_services.outputs.hcp_hvn_cidr_blocks
@@ -23,4 +24,16 @@ module "us_east_1" {
 
   # create routes to TGW for all CIDRs except own VPC
   accessible_cidr_blocks = [for cidr in values(local.accessible_cidr_blocks) : cidr if cidr != local.accessible_cidr_blocks.runtime_ec2_us_east_1]
+}
+
+module "us_west_2" {
+  source = "./region"
+  vpc_cidr_block = local.accessible_cidr_blocks.runtime_ec2_us_west_2
+  region = local.us_west_2
+  transit_gateway_id = local.transit_gateway_ids[local.us_west_2]
+  shared_services_cidr_block = local.shared_services_cidr_blocks[local.us_west_2]
+  hcp_hvn_cidr_block = local.hcp_hvn_cidr_blocks[local.us_west_2]
+
+  # create routes to TGW for all CIDRs except own VPC
+  accessible_cidr_blocks = [for cidr in values(local.accessible_cidr_blocks) : cidr if cidr != local.accessible_cidr_blocks.runtime_ec2_us_west_2]
 }
