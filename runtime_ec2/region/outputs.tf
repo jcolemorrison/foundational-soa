980ef97d5a260aea20fb307b265b500c0be40a23
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-08 14:36:15 -0500

    Add workers to EC2 runtime

---
+++ runtime_ec2/region/outputs.tf
@@ -0,0 +1,10 @@
+output "keypair_name" {
+  value       = aws_key_pair.boundary.key_name
+  description = "Boundary worker keypair"
+}
+
+output "ssh_private_key" {
+  value       = base64encode(tls_private_key.boundary.private_key_openssh)
+  description = "Boundary worker SSH key"
+  sensitive   = true
+}
--- runtime_ec2/region/variables.tf
+++ runtime_ec2/region/variables.tf
@@ -9,22 +9,77 @@ variable "region" {
 }
 
 variable "transit_gateway_id" {
-  type = string
+  type        = string
   description = "transit gateway ID to point traffic to for shared services, hcp, etc."
 }
 
 variable "shared_services_cidr_block" {
-  type = string
+  type        = string
   description = "CIDR block of the shared services sandbox."
 }
 
 variable "hcp_hvn_cidr_block" {
-  type = string
+  type        = string
   description = "CIDR block of the HCP HVN."
 }
 
 variable "accessible_cidr_blocks" {
-  type = list(string)
+  type        = list(string)
   description = "List of CIDR blocks to point to the transit gateway in addition to the Shared Services sandbox and HCP HVN"
-  default = []
+  default     = []
+}
+
+variable "name" {
+  type        = string
+  description = "Name of regional resources"
+}
+
+## Boundary workers
+
+variable "create_boundary_workers" {
+  type        = bool
+  description = "Create Boundary workers, one per public subnet"
+  default     = false
+}
+
+variable "keypair" {
+  type        = string
+  description = "Keypair"
+  default     = null
+}
+
+variable "vault_address" {
+  type        = string
+  description = "Vault cluster address"
+  default     = null
+}
+
+variable "vault_namespace" {
+  type        = string
+  description = "Vault cluster namespace"
+  default     = null
+}
+
+variable "boundary_worker_vault_path" {
+  type        = string
+  description = "Path in Vault for Boundary worker to store secret"
+  default     = null
+}
+
+variable "boundary_worker_vault_token" {
+  type        = string
+  description = "Token in Vault for Boundary worker to store secret"
+  sensitive   = true
+  default     = null
+}
+
+variable "boundary_cluster_id" {
+  type        = string
+  description = "Boundary cluster ID for workers to register"
+  default     = null
+}
+
+variable "boundary_project_scope_id" {
+  type        = string
+  description = "Boundary project scope ID for EKS runtime"
 }
