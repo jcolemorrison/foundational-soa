ac6bb9bbc8fc8c9d4edb63a6ffcf29ea26309919
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-10 14:10:48 -0500

    Add eu-west-1

--- runtime_eks/kubernetes/main.tf
+++ runtime_eks/kubernetes/main.tf
@@ -37,3 +37,23 @@ module "us_west_2" {
     helm       = helm.us_west_2
   }
 }
+
+module "eu_west_1" {
+  source = "./region"
+
+  hcp_consul_cluster_id     = local.eu_west_1.consul.id
+  hcp_vault_private_address = local.eu_west_1.vault.private_address
+  kubernetes_endpoint       = replace(data.aws_eks_cluster.eu_west_1.endpoint, "https://", "")
+
+  hcp_consul_observability = {
+    client_id     = var.hcp_consul_observability.eu_west_1.client_id
+    client_secret = var.hcp_consul_observability.eu_west_1.client_secret
+    resource_id   = var.hcp_consul_observability.eu_west_1.resource_id
+  }
+
+  providers = {
+    aws        = aws.eu_west_1
+    kubernetes = kubernetes.eu_west_1
+    helm       = helm.eu_west_1
+  }
+}
