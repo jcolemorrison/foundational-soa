f579bc5d542ff5a7da6ac7e0b60c04e79e07bfe8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-10 14:01:58 -0500

    Add telemetry

--- runtime_eks/kubernetes/main.tf
+++ runtime_eks/kubernetes/main.tf
@@ -5,6 +5,12 @@ module "us_east_1" {
   hcp_vault_private_address = local.us_east_1.vault.private_address
   kubernetes_endpoint       = replace(data.aws_eks_cluster.us_east_1.endpoint, "https://", "")
 
+  hcp_consul_observability = {
+    client_id     = var.hcp_consul_observability.us_east_1.client_id
+    client_secret = var.hcp_consul_observability.us_east_1.client_secret
+    resource_id   = var.hcp_consul_observability.us_east_1.resource_id
+  }
+
   providers = {
     aws        = aws
     kubernetes = kubernetes.us_east_1
--- runtime_eks/kubernetes/variables.tf
+++ runtime_eks/kubernetes/variables.tf
@@ -2,3 +2,24 @@ variable "terraform_cloud_organization" {
   type        = string
   description = "Terraform Cloud organization that outputs HCP Consul address, token, and datacenter"
 }
+
+variable "hcp_consul_observability" {
+  type = object({
+    us_east_1 = object({
+      client_id     = string
+      client_secret = string
+      resource_id   = string
+    })
+    us_west_2 = object({
+      client_id     = string
+      client_secret = string
+      resource_id   = string
+    })
+    eu_west_1 = object({
+      client_id     = string
+      client_secret = string
+      resource_id   = string
+    })
+  })
+  description = "HCP Consul observability credentials for telemetry collector"
+}
