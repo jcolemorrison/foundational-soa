07c4c8e4f9c6a878e5f33809255469577d71b99e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-10 15:49:50 -0500

    Remove peering through Terraform

--- shared_services/consul/peering.tf
+++ shared_services/consul/peering.tf
@@ -1,38 +1,38 @@
-## Set up Consul cluster peering between us-east-1 and us-west-2
-resource "consul_peering_token" "us_east_1_to_us_west_2" {
-  provider  = consul.us_east_1
-  peer_name = data.terraform_remote_state.shared_services.outputs.hcp_us_west_2.consul.id
-}
+# ## Set up Consul cluster peering between us-east-1 and us-west-2
+# resource "consul_peering_token" "us_east_1_to_us_west_2" {
+#   provider  = consul.us_east_1
+#   peer_name = data.terraform_remote_state.shared_services.outputs.hcp_us_west_2.consul.id
+# }
 
-resource "consul_peering" "us_east_1_to_us_west_2" {
-  provider = consul.us_west_2
+# resource "consul_peering" "us_east_1_to_us_west_2" {
+#   provider = consul.us_west_2
 
-  peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_us_east_1.consul.id
-  peering_token = consul_peering_token.us_east_1_to_us_west_2.peering_token
-}
+#   peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_us_east_1.consul.id
+#   peering_token = consul_peering_token.us_east_1_to_us_west_2.peering_token
+# }
 
-## Set up Consul cluster peering between us-east-1 and eu-west-1
-resource "consul_peering_token" "us_east_1_to_eu_west_1" {
-  provider  = consul.us_east_1
-  peer_name = data.terraform_remote_state.shared_services.outputs.hcp_eu_west_1.consul.id
-}
+# ## Set up Consul cluster peering between us-east-1 and eu-west-1
+# resource "consul_peering_token" "us_east_1_to_eu_west_1" {
+#   provider  = consul.us_east_1
+#   peer_name = data.terraform_remote_state.shared_services.outputs.hcp_eu_west_1.consul.id
+# }
 
-resource "consul_peering" "us_east_1_to_eu_west_1" {
-  provider = consul.eu_west_1
+# resource "consul_peering" "us_east_1_to_eu_west_1" {
+#   provider = consul.eu_west_1
 
-  peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_us_east_1.consul.id
-  peering_token = consul_peering_token.us_east_1_to_eu_west_1.peering_token
-}
+#   peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_us_east_1.consul.id
+#   peering_token = consul_peering_token.us_east_1_to_eu_west_1.peering_token
+# }
 
-## Set up Consul cluster peering between eu-west-1 and us-west-2
-resource "consul_peering_token" "eu_west_1_to_us_west_2" {
-  provider  = consul.eu_west_1
-  peer_name = data.terraform_remote_state.shared_services.outputs.hcp_us_west_2.consul.id
-}
+# ## Set up Consul cluster peering between eu-west-1 and us-west-2
+# resource "consul_peering_token" "eu_west_1_to_us_west_2" {
+#   provider  = consul.eu_west_1
+#   peer_name = data.terraform_remote_state.shared_services.outputs.hcp_us_west_2.consul.id
+# }
 
-resource "consul_peering" "eu_west_1_to_us_west_2" {
-  provider = consul.us_west_2
+# resource "consul_peering" "eu_west_1_to_us_west_2" {
+#   provider = consul.us_west_2
 
-  peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_eu_west_1.consul.id
-  peering_token = consul_peering_token.eu_west_1_to_us_west_2.peering_token
-}
+#   peer_name     = data.terraform_remote_state.shared_services.outputs.hcp_eu_west_1.consul.id
+#   peering_token = consul_peering_token.eu_west_1_to_us_west_2.peering_token
+# }
--- shared_services/consul/telemetry.tf
+++ shared_services/consul/telemetry.tf
@@ -1,33 +1,53 @@
-resource "consul_config_entry_service_intentions" "telemetry_us_east_1" {
+resource "consul_config_entry" "telemetry_us_east_1" {
   name = "consul-telemetry-collector"
+  kind = "service-intentions"
 
-  sources {
-    name   = "*"
-    action = "allow"
-  }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "*"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
 
   provider = consul.us_east_1
 }
 
-resource "consul_config_entry_service_intentions" "telemetry_us_west_2" {
+resource "consul_config_entry" "telemetry_us_west_2" {
   name = "consul-telemetry-collector"
+  kind = "service-intentions"
 
-  sources {
-    name   = "*"
-    action = "allow"
-  }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "*"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
 
   provider = consul.us_west_2
 }
 
-
-resource "consul_config_entry_service_intentions" "telemetry_eu_west_1" {
+resource "consul_config_entry" "telemetry_eu_west_1" {
   name = "consul-telemetry-collector"
+  kind = "service-intentions"
 
-  sources {
-    name   = "*"
-    action = "allow"
-  }
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "*"
+        Precedence = 9
+        Type       = "consul"
+      }
+    ]
+  })
 
   provider = consul.eu_west_1
 }
