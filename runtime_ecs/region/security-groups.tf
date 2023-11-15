ae84f0364a230427340322912028b72f93c1e3e8
J Cole Morrison <me@jcolemorrison.com>
2023-11-15 00:38:24 -0800

    security group rule changes for testing

--- runtime_ecs/region/security-groups.tf
+++ runtime_ecs/region/security-groups.tf
@@ -35,8 +35,8 @@ resource "aws_security_group_rule" "consul_client_allow_inbound_HCP_8301_tcp" {
   type              = "ingress"
   protocol          = "tcp"
   cidr_blocks       = [ var.hcp_hvn_cidr_block ]
-  from_port         = 8301
-  to_port           = 8301
+  from_port         = 0
+  to_port           = 65535
   description       = "Allow TCP traffic from HCP with this security group."
 }
 
@@ -45,8 +45,8 @@ resource "aws_security_group_rule" "consul_client_allow_inbound_HCP_8301_udp" {
   type              = "ingress"
   protocol          = "udp"
   cidr_blocks       = [ var.hcp_hvn_cidr_block ]
-  from_port         = 8301
-  to_port           = 8301
+  from_port         = 0
+  to_port           = 65535
   description       = "Allow UDP traffic from HCP with this security group."
 }
 
