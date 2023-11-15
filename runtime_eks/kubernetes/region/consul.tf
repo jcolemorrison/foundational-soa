3c59a1d1e7573a9c61650fb1958735a285cb8681
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-14 20:33:08 -0500

    Remove partition

--- runtime_eks/kubernetes/region/consul.tf
+++ runtime_eks/kubernetes/region/consul.tf
@@ -100,34 +100,3 @@ resource "helm_release" "consul_client" {
     value = "hashicorp/consul:${replace(data.hcp_consul_cluster.cluster.consul_version, "v", "")}"
   }
 }
-
-resource "helm_release" "consul_partition" {
-  depends_on       = [kubernetes_secret.hcp_consul_secret, kubernetes_secret.hcp_consul_token]
-  name             = "consul-eks"
-  namespace        = kubernetes_namespace.consul.metadata.0.name
-  create_namespace = false
-
-  repository = "https://helm.releases.hashicorp.com"
-  chart      = "consul"
-  version    = var.consul_helm_version
-
-  values = [
-    data.hcp_consul_agent_helm_config.cluster.config,
-    file("${path.module}/templates/partition.yaml"),
-  ]
-
-  set {
-    name  = "global.image"
-    value = "hashicorp/consul:${replace(data.hcp_consul_cluster.cluster.consul_version, "v", "")}"
-  }
-
-  set {
-    name  = "client.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "controller.enabled"
-    value = "false"
-  }
-}
