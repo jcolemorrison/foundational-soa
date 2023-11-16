66e79cf537bad28be82a541cf7b1d70b3e618d9c
J Cole Morrison <me@jcolemorrison.com>
2023-11-15 21:51:24 -0800

    add tfc cloud org variable

--- runtime_ecs/applications/variables.tf
+++ runtime_ecs/applications/variables.tf
@@ -3,3 +3,8 @@ locals {
   us_west_2 = "us-west-2"
   eu_west_1 = "eu-west-1"
 }
+
+variable "terraform_cloud_organization" {
+  type        = string
+  description = "Terraform Cloud organization that outputs HCP Consul address, token, and datacenter"
+}
