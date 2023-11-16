3ce72ef1400d1fd9750326963387331719c9639e
J Cole Morrison <me@jcolemorrison.com>
2023-11-15 22:51:50 -0800

    intentions for eu west services

--- runtime_ecs/applications/service-intentions.tf
+++ runtime_ecs/applications/service-intentions.tf
@@ -1,37 +1,3 @@
-# resource "consul_config_entry" "name" {
-#   kind = "service-intentions"
-#   name = "${local.us_east_1}-ecs-api"
-#   namespace = "default"
-#   partition = "ecs"
-#   config_json = jsonencode({
-#     Sources = [
-#       {
-#         Name = "${local.us_east_1}-ecs-upstream"
-#         Action = "allow"
-#         # Peer = "prod-${local.us_east_1}-ecs"
-#         Namespace = "default"
-#         Partition = "ecs"
-#       },
-#       {
-#         Name = "${local.us_west_2}-ecs-upstream"
-#         Action = "allow"
-#         Peer = "prod-${local.us_west_2}-ecs"
-#         Namespace = "default"
-#         # Partition = "ecs"
-#       },
-#       {
-#         Name = "${local.eu_west_1}-ecs-upstream"
-#         Action = "allow"
-#         Peer = "prod-${local.eu_west_1}-ecs"
-#         Namespace = "default"
-#         # Partition = "ecs"
-#       }
-#     ]
-#   })
-
-#   provider = consul.us_east_1
-# }
-
 resource "consul_config_entry" "us_east_1_api_to_upstreams" {
   kind = "service-intentions"
   name = "${local.us_east_1}-ecs-upstream"
@@ -98,4 +64,38 @@ resource "consul_config_entry" "us_west_2_api_to_upstreams" {
   })
 
   provider = consul.us_west_2
+}
+
+resource "consul_config_entry" "eu_west_1_api_to_upstreams" {
+  kind = "service-intentions"
+  name = "${local.eu_west_1}-ecs-upstream"
+  namespace = "default"
+  partition = "ecs"
+  config_json = jsonencode({
+    Sources = [
+      {
+        Name = "${local.us_east_1}-ecs-api"
+        Action = "allow"
+        Peer = "prod-${local.us_east_1}-ecs"
+        Namespace = "default"
+        # Partition = "ecs"
+      },
+      {
+        Name = "${local.us_west_2}-ecs-api"
+        Action = "allow"
+        Peer = "prod-${local.us_west_2}-ecs"
+        Namespace = "default"
+        # Partition = "ecs"
+      },
+      {
+        Name = "${local.eu_west_1}-ecs-api"
+        Action = "allow"
+        # Peer = "prod-${local.eu_west_1}-ecs"
+        Namespace = "default"
+        Partition = "ecs"
+      }
+    ]
+  })
+
+  provider = consul.eu_west_1
 }
