f67ff6702d0dc614444f489bec7bbdd4203b0027
J Cole Morrison <me@jcolemorrison.com>
2023-11-15 22:26:38 -0800

    reverse intention

--- runtime_ecs/applications/service-intentions.tf
+++ runtime_ecs/applications/service-intentions.tf
@@ -1,26 +1,60 @@
+# resource "consul_config_entry" "name" {
+#   kind = "service-intentions"
+#   name = "${local.us_east_1}-ecs-api"
+#   namespace = "default"
+#   partition = "ecs"
+#   config_json = jsonencode({
+#     Sources = [
+#       {
+#         Name = "${local.us_east_1}-ecs-upstream"
+#         Action = "allow"
+#         # Peer = "prod-${local.us_east_1}-ecs"
+#         Namespace = "default"
+#         Partition = "ecs"
+#       },
+#       {
+#         Name = "${local.us_west_2}-ecs-upstream"
+#         Action = "allow"
+#         Peer = "prod-${local.us_west_2}-ecs"
+#         Namespace = "default"
+#         # Partition = "ecs"
+#       },
+#       {
+#         Name = "${local.eu_west_1}-ecs-upstream"
+#         Action = "allow"
+#         Peer = "prod-${local.eu_west_1}-ecs"
+#         Namespace = "default"
+#         # Partition = "ecs"
+#       }
+#     ]
+#   })
+
+#   provider = consul.us_east_1
+# }
+
 resource "consul_config_entry" "name" {
   kind = "service-intentions"
-  name = "${local.us_east_1}-ecs-api"
+  name = "${local.us_east_1}-ecs-upstream"
   namespace = "default"
   partition = "ecs"
   config_json = jsonencode({
     Sources = [
       {
-        Name = "${local.us_east_1}-ecs-upstream"
+        Name = "${local.us_east_1}-ecs-api"
         Action = "allow"
-        Peer = "prod-${local.us_east_1}-ecs"
+        # Peer = "prod-${local.us_east_1}-ecs"
         Namespace = "default"
-        # Partition = "ecs"
+        Partition = "ecs"
       },
       {
-        Name = "${local.us_west_2}-ecs-upstream"
+        Name = "${local.us_west_2}-ecs-api"
         Action = "allow"
         Peer = "prod-${local.us_west_2}-ecs"
         Namespace = "default"
         # Partition = "ecs"
       },
       {
-        Name = "${local.eu_west_1}-ecs-upstream"
+        Name = "${local.eu_west_1}-ecs-api"
         Action = "allow"
         Peer = "prod-${local.eu_west_1}-ecs"
         Namespace = "default"
