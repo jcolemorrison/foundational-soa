aa9203e6a9aca2eb4d6674939caa609df5f442a6
J Cole Morrison <me@jcolemorrison.com>
2023-11-16 16:39:56 -0800

    switch from sameness groups

--- runtime_ecs/applications/us-east-1.tf
+++ runtime_ecs/applications/us-east-1.tf
@@ -1,62 +1,3 @@
-## Sameness Groups
-
-resource "consul_config_entry" "us_east_1_ecs_sameness_group" {
-  kind      = "sameness-group"
-  name      = "${local.us_east_1}-ecs-sameness-group"
-  partition = "ecs"
-
-  config_json = jsonencode({
-    DefaultForFailover = false
-    IncludeLocal       = true
-    Members = [
-      { Peer = "${local.dc_us_west_2}-ecs" },
-      { Peer = "${local.dc_eu_west_1}-ecs" }
-    ]
-  })
-
-  provider = consul.us_east_1
-}
-
-## Service Intentions
-
-resource "consul_config_entry" "us_east_1_api_to_upstreams" {
-  kind = "service-intentions"
-  name = "ecs-upstream"
-  namespace = "default"
-  partition = "ecs"
-  config_json = jsonencode({
-    Sources = [
-      {
-        Name = "ecs-api"
-        Action = "allow"
-        # Peer = "prod-${local.us_east_1}-ecs"
-        Namespace = "default"
-        # Partition = "ecs"
-        SamenessGroup = "${local.us_east_1}-ecs-sameness-group"
-      }
-      # {
-      #   Name = "${local.us_west_2}-ecs-api"
-      #   Action = "allow"
-      #   Peer = "${local.dc_us_west_2}-ecs" # Format = "{peer}-{partition}" i.e. "prod-us-west-2-ecs" where "prod-us-west-2" = peer and "ecs" = partition
-      #   Namespace = "default"
-      #   # Partition = "ecs"
-      #   # SamenessGroup = "${local.us_east_1}-ecs-sameness-group"
-      # },
-      # {
-      #   Name = "${local.eu_west_1}-ecs-api"
-      #   Action = "allow"
-      #   Peer = "${local.dc_eu_west_1}-ecs"
-      #   Namespace = "default"
-      #   # Partition = "ecs"
-      # }
-    ]
-  })
-
-  provider = consul.us_east_1
-}
-
-## Service Resolvers
-
 resource "consul_config_entry_service_resolver" "us_east_1_upstream_test" {
   name = "ecs-upstream" # name of service this applies to, despite inaccurate docs
   namespace = "default"
@@ -72,3 +13,78 @@ resource "consul_config_entry_service_resolver" "us_east_1_upstream_test" {
 
   provider = consul.us_east_1
 }
+
+# ## Sameness Groups
+
+# resource "consul_config_entry" "us_east_1_ecs_sameness_group" {
+#   kind      = "sameness-group"
+#   name      = "${local.us_east_1}-ecs-sameness-group"
+#   partition = "ecs"
+
+#   config_json = jsonencode({
+#     DefaultForFailover = false
+#     IncludeLocal       = true
+#     Members = [
+#       { Peer = "${local.dc_us_west_2}-ecs" },
+#       { Peer = "${local.dc_eu_west_1}-ecs" }
+#     ]
+#   })
+
+#   provider = consul.us_east_1
+# }
+
+# ## Service Intentions
+
+# resource "consul_config_entry" "us_east_1_api_to_upstreams" {
+#   kind = "service-intentions"
+#   name = "ecs-upstream"
+#   namespace = "default"
+#   partition = "ecs"
+#   config_json = jsonencode({
+#     Sources = [
+#       {
+#         Name = "ecs-api"
+#         Action = "allow"
+#         # Peer = "prod-${local.us_east_1}-ecs"
+#         Namespace = "default"
+#         # Partition = "ecs"
+#         SamenessGroup = "${local.us_east_1}-ecs-sameness-group"
+#       }
+#       # {
+#       #   Name = "${local.us_west_2}-ecs-api"
+#       #   Action = "allow"
+#       #   Peer = "${local.dc_us_west_2}-ecs" # Format = "{peer}-{partition}" i.e. "prod-us-west-2-ecs" where "prod-us-west-2" = peer and "ecs" = partition
+#       #   Namespace = "default"
+#       #   # Partition = "ecs"
+#       #   # SamenessGroup = "${local.us_east_1}-ecs-sameness-group"
+#       # },
+#       # {
+#       #   Name = "${local.eu_west_1}-ecs-api"
+#       #   Action = "allow"
+#       #   Peer = "${local.dc_eu_west_1}-ecs"
+#       #   Namespace = "default"
+#       #   # Partition = "ecs"
+#       # }
+#     ]
+#   })
+
+#   provider = consul.us_east_1
+# }
+
+# ## Service Resolvers
+
+# resource "consul_config_entry_service_resolver" "us_east_1_upstream_test" {
+#   name = "ecs-upstream" # name of service this applies to, despite inaccurate docs
+#   namespace = "default"
+#   partition = "ecs"
+#   connect_timeout = "1s" # required fields
+#   request_timeout = "1s"
+
+#   redirect {
+#     service = "ecs-upstream"
+#     peer = "${local.dc_us_west_2}-ecs"
+#     namespace = "default"
+#   }
+
+#   provider = consul.us_east_1
+# }
--- runtime_ecs/applications/us-west-2.tf
+++ runtime_ecs/applications/us-west-2.tf
@@ -1,24 +1,3 @@
-## Sameness Groups
-
-resource "consul_config_entry" "us_west_2_ecs_sameness_group" {
-  kind      = "sameness-group"
-  name      = "${local.us_west_2}-ecs-sameness-group"
-  partition = "ecs"
-
-  config_json = jsonencode({
-    DefaultForFailover = true
-    IncludeLocal       = true
-    Members = [
-      { Peer = "${local.dc_us_east_1}-ecs" },
-      { Peer = "${local.dc_eu_west_1}-ecs" }
-    ]
-  })
-
-  provider = consul.us_west_2
-}
-
-## Service Exports
-
 resource "consul_config_entry" "us_west_2_export_upstream" {
   kind = "exported-services"
   name = "ecs"
@@ -31,7 +10,7 @@ resource "consul_config_entry" "us_west_2_export_upstream" {
         Name = "ecs-upstream"
         Consumers = [
           {
-            SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
+            Peer = "${local.dc_us_east_1}-ecs"
           }
         ]
       }
@@ -41,7 +20,7 @@ resource "consul_config_entry" "us_west_2_export_upstream" {
   provider = consul.us_west_2
 }
 
-## Service Intentions
+# ## Service Intentions
 
 resource "consul_config_entry" "us_west_2_api_to_upstreams" {
   kind = "service-intentions"
@@ -50,31 +29,98 @@ resource "consul_config_entry" "us_west_2_api_to_upstreams" {
   partition = "ecs"
   config_json = jsonencode({
     Sources = [
-      # {
-      #   Name = "${local.us_east_1}-ecs-api"
-      #   Action = "allow"
-      #   Peer = "${local.dc_us_east_1}-ecs"
-      #   Namespace = "default"
-      #   # Partition = "ecs"
-      #   # SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
-      # },
       {
         Name = "ecs-api"
         Action = "allow"
-        # Peer = "prod-${local.us_west_2}-ecs"
+        Peer = "${local.dc_us_east_1}-ecs"
         Namespace = "default"
         # Partition = "ecs"
-        SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
+        # SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
       }
-      # {
-      #   Name = "${local.eu_west_1}-ecs-api"
-      #   Action = "allow"
-      #   Peer = "${local.dc_eu_west_1}-ecs"
-      #   Namespace = "default"
-      #   # Partition = "ecs"
-      # }
     ]
   })
 
   provider = consul.us_west_2
 }
+
+
+# ## Sameness Groups
+
+# resource "consul_config_entry" "us_west_2_ecs_sameness_group" {
+#   kind      = "sameness-group"
+#   name      = "${local.us_west_2}-ecs-sameness-group"
+#   partition = "ecs"
+
+#   config_json = jsonencode({
+#     DefaultForFailover = true
+#     IncludeLocal       = true
+#     Members = [
+#       { Peer = "${local.dc_us_east_1}-ecs" },
+#       { Peer = "${local.dc_eu_west_1}-ecs" }
+#     ]
+#   })
+
+#   provider = consul.us_west_2
+# }
+
+# ## Service Exports
+
+# resource "consul_config_entry" "us_west_2_export_upstream" {
+#   kind = "exported-services"
+#   name = "ecs"
+#   partition = "ecs"
+
+#   config_json = jsonencode({
+#     # Name = "default"
+#     Services = [
+#       {
+#         Name = "ecs-upstream"
+#         Consumers = [
+#           {
+#             SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
+#           }
+#         ]
+#       }
+#     ]
+#   })
+
+#   provider = consul.us_west_2
+# }
+
+# ## Service Intentions
+
+# resource "consul_config_entry" "us_west_2_api_to_upstreams" {
+#   kind = "service-intentions"
+#   name = "ecs-upstream"
+#   namespace = "default"
+#   partition = "ecs"
+#   config_json = jsonencode({
+#     Sources = [
+#       # {
+#       #   Name = "${local.us_east_1}-ecs-api"
+#       #   Action = "allow"
+#       #   Peer = "${local.dc_us_east_1}-ecs"
+#       #   Namespace = "default"
+#       #   # Partition = "ecs"
+#       #   # SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
+#       # },
+#       {
+#         Name = "ecs-api"
+#         Action = "allow"
+#         # Peer = "prod-${local.us_west_2}-ecs"
+#         Namespace = "default"
+#         # Partition = "ecs"
+#         SamenessGroup = "${local.us_west_2}-ecs-sameness-group"
+#       }
+#       # {
+#       #   Name = "${local.eu_west_1}-ecs-api"
+#       #   Action = "allow"
+#       #   Peer = "${local.dc_eu_west_1}-ecs"
+#       #   Namespace = "default"
+#       #   # Partition = "ecs"
+#       # }
+#     ]
+#   })
+
+#   provider = consul.us_west_2
+# }
