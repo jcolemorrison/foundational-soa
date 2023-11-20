290f234deabb05cb3fbd37d44443202078398b1a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-20 09:55:26 -0500

    Remove use of taint to replace tokens

--- shared_services/scripts/setup.sh
+++ shared_services/scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-terraform taint 'module.hcp_eu_west_1.hcp_vault_cluster_admin_token.vault[0]'
-terraform taint 'module.hcp_us_east_1.hcp_vault_cluster_admin_token.vault[0]'
-terraform taint 'module.hcp_us_west_2.hcp_vault_cluster_admin_token.vault[0]'
+terraform apply -replace='module.hcp_eu_west_1.hcp_vault_cluster_admin_token.vault[0]'
+terraform apply -replace='module.hcp_us_east_1.hcp_vault_cluster_admin_token.vault[0]'
+terraform apply -replace='module.hcp_us_west_2.hcp_vault_cluster_admin_token.vault[0]'
