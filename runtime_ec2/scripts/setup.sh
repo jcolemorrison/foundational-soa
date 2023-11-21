457075f0a24c4e68ea0af180a1fe4f111c16e2e0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-21 12:42:23 -0500

    Add script to reset workers

---
+++ runtime_ec2/scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+terraform taint 'module.us_east_1.module.boundary_worker[0].aws_instance.worker'
+terraform taint 'module.us_west_2.module.boundary_worker[0].aws_instance.worker'
+terraform taint 'module.eu_west_1.module.boundary_worker[0].aws_instance.worker'
+
+terraform taint 'module.us_east_1.boundary_worker.ec2[0]'
+terraform taint 'module.us_west_2.boundary_worker.ec2[0]'
+terraform taint 'module.eu_west_1.boundary_worker.ec2[0]'
