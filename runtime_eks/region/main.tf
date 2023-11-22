68e0ddf1cacc776585276fec761a73aac8684df3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-22 10:56:40 -0500

    Revert "Remove everything on networks"
    This reverts commit 8c4d88d48c50ebf2abb7c73460514d8e0310a7a0.

--- runtime_eks/region/main.tf
+++ runtime_eks/region/main.tf
@@ -20,20 +20,20 @@ module "boundary_worker" {
   runtime             = var.runtime
 }
 
-# # EKS
-# module "eks" {
-#   count  = var.create_eks_cluster ? 1 : 0
-#   source = "../modules/eks"
+# EKS
+module "eks" {
+  count  = var.create_eks_cluster ? 1 : 0
+  source = "../modules/eks"
 
-#   name = var.name
+  name = var.name
 
-#   vpc_id                 = module.network.vpc_id
-#   private_subnet_ids     = module.network.vpc_private_subnet_ids
-#   hcp_network_cidr_block = var.hcp_hvn_cidr_block
-#   accessible_cidr_blocks = var.accessible_cidr_blocks
+  vpc_id                 = module.network.vpc_id
+  private_subnet_ids     = module.network.vpc_private_subnet_ids
+  hcp_network_cidr_block = var.hcp_hvn_cidr_block
+  accessible_cidr_blocks = var.accessible_cidr_blocks
 
-#   remote_access = {
-#     ec2_ssh_key               = aws_key_pair.boundary.key_name
-#     source_security_group_ids = [module.boundary_worker.0.security_group_id]
-#   }
-# }
+  remote_access = {
+    ec2_ssh_key               = aws_key_pair.boundary.key_name
+    source_security_group_ids = [module.boundary_worker.0.security_group_id]
+  }
+}
