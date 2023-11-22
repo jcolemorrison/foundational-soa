fac403f393b0a1adb0361e8a45cb855f3c3d9590
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-22 15:12:34 -0500

    Add scripts to load data into database

--- modules/vault/service_secrets/outputs.tf
+++ modules/vault/service_secrets/outputs.tf
@@ -4,15 +4,23 @@ output "namespace" {
 }
 
 output "static_secrets_path" {
-  value = vault_mount.static.path
+  value       = vault_mount.static.path
+  description = "Path to secrets in key-value store, including database administrative password"
 }
 
 output "database_secrets_path" {
-  value = var.db_name != null ? vault_mount.database.0.path : null
+  value       = var.db_name != null ? vault_mount.database.0.path : null
+  description = "Path to database credentials"
+}
+
+output "database_secrets_role" {
+  value       = var.db_name != null ? vault_database_secret_backend_role.database.0.name : null
+  description = "Vault role for database credentials"
 }
 
 output "policies" {
   value = concat([
     vault_policy.static.name
   ], var.db_name != null ? [vault_policy.database.0.name] : [])
+  description = "List of Vault policies for static and dynamic secrets"
 }
--- runtime_eks/scripts/db.sh
+++ runtime_eks/scripts/db.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 mkdir -p secrets
-@echo "$(cd shared_services && terraform output -json hcp_us_east_1 | jq -r .boundary.password)" > secrets/boundary_admin
-boundary authenticate password -login-name=$(cd shared_services && terraform output -json hcp_us_east_1 | jq -r .boundary.username) \
-    -password file://secrets/admin
+echo "$(cd shared_services && terraform output -json hcp_us_east_1 | jq -r .boundary.password)" > secrets/boundary_admin
 
+boundary authenticate password \
+    -login-name=$(cd shared_services && terraform output -json hcp_us_east_1 | jq -r .boundary.username) \
+    -password file://secrets/boundary_admin
+
+boundary connect postgres \
+    -dbname=customers \
+    -target-name database-admin-customers \
+    -target-scope-name=runtime_eks \
+    -- -f runtime_eks/scripts/setup.sql
