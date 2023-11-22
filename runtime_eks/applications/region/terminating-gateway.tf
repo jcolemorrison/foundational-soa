c1c6ea65b6ddb493c40c6313d91f8471e8ab2f03
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-22 16:49:47 -0500

    Add terminating gateway linking

--- runtime_eks/applications/region/terminating-gateway.tf
+++ runtime_eks/applications/region/terminating-gateway.tf
@@ -15,3 +15,21 @@ resource "consul_acl_role_policy_attachment" "terminating_gateway_database" {
   role_id = data.consul_acl_role.terminating_gateway.id
   policy  = consul_acl_policy.terminating_gateway_database.name
 }
+
+resource "kubernetes_manifest" "terminating_gateway" {
+  manifest = {
+    "apiVersion" = "consul.hashicorp.com/v1alpha1"
+    "kind"       = "TerminatingGateway"
+    "metadata" = {
+      "name"      = "terminating-gateway"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "services" = [
+        {
+          "name" = "${var.service_name}-database"
+        },
+      ]
+    }
+  }
+}
