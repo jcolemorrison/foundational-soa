c682721f7d4aa50809ab9c3fa25353f36d25d15f
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-22 20:52:30 -0500

    Remove database exported service

--- runtime_eks/applications/eu-west-1.tf
+++ runtime_eks/applications/eu-west-1.tf
@@ -19,29 +19,3 @@ module "fake_service_eu_west_1" {
     consul     = consul.eu_west_1
   }
 }
-
-resource "kubernetes_manifest" "exported_services_default_eu_west_1" {
-  manifest = {
-    "apiVersion" = "consul.hashicorp.com/v1alpha1"
-    "kind"       = "ExportedServices"
-    "metadata" = {
-      "name"      = "default"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "services" = [
-        {
-          "consumers" = [
-            {
-              "samenessGroup" = module.fake_service_eu_west_1.sameness_group
-            },
-          ]
-          "name" = "database"
-        },
-      ]
-    }
-  }
-
-
-  provider = kubernetes.eu_west_1
-}
