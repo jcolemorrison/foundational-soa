0616826a35d297ef7801fe02f1e6501b441011f3
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-23 21:32:31 -0500

    Move Vault namespace creation to top level

--- runtime_eks/region/database.tf
+++ runtime_eks/region/database.tf
@@ -62,6 +62,16 @@ resource "consul_config_entry" "service_defaults" {
   })
 }
 
-resource "vault_namespace" "database" {
-  path = var.name
-}
+# module "database_vault" {
+#   source      = "../modules/vault/service_secrets"
+#   service     = local.name
+#   db_name     = module.us_east_1.database.dbname
+#   db_username = module.us_east_1.database.username
+#   db_password = module.us_east_1.database.password
+#   db_address  = module.us_east_1.database.address
+#   db_port     = module.us_east_1.database.port
+
+#   providers = {
+#     vault = vault.admin
+#   }
+# }
--- runtime_eks/vault.tf
+++ runtime_eks/vault.tf
@@ -1,16 +1,7 @@
-# module "database_vault" {
-#   source      = "../modules/vault/service_secrets"
-#   service     = local.name
-#   db_name     = module.us_east_1.database.dbname
-#   db_username = module.us_east_1.database.username
-#   db_password = module.us_east_1.database.password
-#   db_address  = module.us_east_1.database.address
-#   db_port     = module.us_east_1.database.port
-
-#   providers = {
-#     vault = vault.admin
-#   }
-# }
+resource "vault_namespace" "database" {
+  path     = local.name
+  provider = vault.us_east_1
+}
 
 # resource "vault_policy" "boundary_controller" {
 #   name      = "boundary-controller"
