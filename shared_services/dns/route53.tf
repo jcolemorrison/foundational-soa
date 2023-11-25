e96f58f8ed127e7d034cd689d124e45e589c8396
J Cole Morrison <me@jcolemorrison.com>
2023-11-24 23:50:21 -0800

    remove unused routing

--- shared_services/dns/route53.tf
+++ shared_services/dns/route53.tf
@@ -9,19 +9,3 @@ resource "aws_route53_record" "subdomain_ecs" {
   ttl     = "30"
   records = data.terraform_remote_state.runtime_ecs.outputs.subdomain_name_servers
 }
-
-# Latency based routing
-# resource "aws_route53_record" "ecs_public_api" {
-#   for_each = local.ecs_public_api_dns_names
-
-#   zone_id = data.aws_route53_zone.main.zone_id
-#   name    = "ecs.${var.domain_name}"
-#   type    = "A"
-#   ttl     = "300"
-#   records = [each.value]
-#   set_identifier = "${each.key}-ecs-public-api"
-
-#   latency_routing_policy {
-#     region = each.key
-#   }
-# }
