d49c546ad654757d18b7ab5df7edef641d19685c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-25 09:27:43 -0500

    Ignore VSO secrets annotations

--- runtime_eks/applications/provider.tf
+++ runtime_eks/applications/provider.tf
@@ -38,6 +38,9 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.us_east_1.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.us_east_1.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.us_east_1.token
+  ignore_annotations = [
+    "vso\\.secrets\\.hashicorp\\.com\\/restartedAt"
+  ]
 }
 
 provider "kubernetes" {
@@ -45,6 +48,9 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.us_west_2.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.us_west_2.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.us_west_2.token
+  ignore_annotations = [
+    "vso\\.secrets\\.hashicorp\\.com\\/restartedAt"
+  ]
 }
 
 provider "kubernetes" {
@@ -52,6 +58,9 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eu_west_1.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eu_west_1.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.eu_west_1.token
+  ignore_annotations = [
+    "vso\\.secrets\\.hashicorp\\.com\\/restartedAt"
+  ]
 }
 
 provider "vault" {
