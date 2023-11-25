a8aad6fc9b7a23d0f01e6c5c6fc648b6dd7d5b61
J Cole Morrison <me@jcolemorrison.com>
2023-11-24 21:54:18 -0800

    for each typo

--- shared_services/dns/route53.tf
+++ shared_services/dns/route53.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "subdomain_ecs" {
 
 # Latency based routing
 resource "aws_route53_record" "ecs_public_api" {
-  for_each = ecs_public_api_dns_names
+  for_each = local.ecs_public_api_dns_names
 
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "ecs.${var.domain_name}"
