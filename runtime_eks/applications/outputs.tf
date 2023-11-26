c3ad0d964dac2e83ae9fa624efe806c125839813
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-26 18:25:39 -0500

    Output to port 80

--- runtime_eks/applications/outputs.tf
+++ runtime_eks/applications/outputs.tf
@@ -24,9 +24,9 @@ data "kubernetes_service" "store_eu_west_1" {
 
 output "store_endpoints" {
   value = {
-    us_east_1 = "http://${data.kubernetes_service.store_us_east_1.status[0].load_balancer[0].ingress[0].hostname}:9090"
-    us_west_2 = "http://${data.kubernetes_service.store_us_west_2.status[0].load_balancer[0].ingress[0].hostname}:9090"
-    eu_west_1 = "http://${data.kubernetes_service.store_eu_west_1.status[0].load_balancer[0].ingress[0].hostname}:9090"
+    us_east_1 = "http://${data.kubernetes_service.store_us_east_1.status[0].load_balancer[0].ingress[0].hostname}"
+    us_west_2 = "http://${data.kubernetes_service.store_us_west_2.status[0].load_balancer[0].ingress[0].hostname}"
+    eu_west_1 = "http://${data.kubernetes_service.store_eu_west_1.status[0].load_balancer[0].ingress[0].hostname}"
   }
   description = "Endpoints for store service"
 }
