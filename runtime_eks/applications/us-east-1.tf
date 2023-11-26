420bf3bc2ea883baea16b794e09b131e817c315e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-26 00:28:31 -0500

    Reset testing for upstream

--- runtime_eks/applications/us-east-1.tf
+++ runtime_eks/applications/us-east-1.tf
@@ -3,7 +3,7 @@ module "fake_service_us_east_1" {
 
   region                  = "us-east-1"
   namespace               = var.namespace
-  test_failover_customers = true
+  test_failover_customers = false
 
   peers_for_failover = [local.peers.us_west_2, local.peers.eu_west_1]
 
