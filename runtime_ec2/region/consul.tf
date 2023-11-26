f83b3bb19ac3c157fa64a51dd5032b1e54467403
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-25 23:49:56 -0500

    Export services

---
+++ runtime_ec2/region/consul.tf
@@ -0,0 +1,15 @@
+resource "consul_config_entry" "exported_services_payments_ec2" {
+  name      = "ec2"
+  kind      = "exported-services"
+  partition = "ec2"
+
+  config_json = jsonencode({
+    Services = [{
+      Name      = "payments"
+      Namespace = "default"
+      Consumers = [{
+        Partition = "default"
+      }]
+    }]
+  })
+}
