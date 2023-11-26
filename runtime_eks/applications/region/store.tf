49e2bc2087b6fcd860246b6dbb8c0c70876c4513
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-26 13:30:18 -0500

    Remove API gatweay

--- runtime_eks/applications/region/store.tf
+++ runtime_eks/applications/region/store.tf
@@ -1,96 +1,96 @@
-resource "kubernetes_manifest" "reference_grant" {
-  manifest = {
-    "apiVersion" = "gateway.networking.k8s.io/v1beta1"
-    "kind"       = "ReferenceGrant"
-    "metadata" = {
-      "name"      = "consul-reference-grant"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "from" = [
-        {
-          "group"     = "gateway.networking.k8s.io"
-          "kind"      = "HTTPRoute"
-          "namespace" = "consul"
-        },
-      ]
-      "to" = [
-        {
-          "group" = ""
-          "kind"  = "Service"
-        },
-      ]
-    }
-  }
-}
+# resource "kubernetes_manifest" "reference_grant" {
+#   manifest = {
+#     "apiVersion" = "gateway.networking.k8s.io/v1beta1"
+#     "kind"       = "ReferenceGrant"
+#     "metadata" = {
+#       "name"      = "consul-reference-grant"
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "from" = [
+#         {
+#           "group"     = "gateway.networking.k8s.io"
+#           "kind"      = "HTTPRoute"
+#           "namespace" = "consul"
+#         },
+#       ]
+#       "to" = [
+#         {
+#           "group" = ""
+#           "kind"  = "Service"
+#         },
+#       ]
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "route_retry_filter" {
-  manifest = {
-    "apiVersion" = "consul.hashicorp.com/v1alpha1"
-    "kind"       = "RouteRetryFilter"
-    "metadata" = {
-      "name"      = "route-root"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "numRetries" = 10
-      "retryOn" = [
-        "5xx",
-        "connect-failure"
-      ]
-      "retryOnConnectFailure" = true
-    }
-  }
-}
+# resource "kubernetes_manifest" "route_retry_filter" {
+#   manifest = {
+#     "apiVersion" = "consul.hashicorp.com/v1alpha1"
+#     "kind"       = "RouteRetryFilter"
+#     "metadata" = {
+#       "name"      = "route-root"
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "numRetries" = 10
+#       "retryOn" = [
+#         "5xx",
+#         "connect-failure"
+#       ]
+#       "retryOnConnectFailure" = true
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "http_route" {
-  manifest = {
-    "apiVersion" = "gateway.networking.k8s.io/v1beta1"
-    "kind"       = "HTTPRoute"
-    "metadata" = {
-      "name"      = "route-root"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "parentRefs" = [
-        {
-          "name"      = "api-gateway"
-          "namespace" = "consul"
-        },
-      ]
-      "rules" = [
-        {
-          "backendRefs" = [
-            {
-              "kind"      = "Service"
-              "name"      = "store"
-              "namespace" = var.namespace
-              "port"      = local.service_ports.store
-            },
-          ]
-          "matches" = [
-            {
-              "path" = {
-                "type"  = "PathPrefix"
-                "value" = "/"
-              }
-            },
-          ]
-          "filters" = [
-            {
-              "extensionRef" = {
-                "group" = "consul.hashicorp.com"
-                "kind"  = "RouteRetryFilter"
-                "name"  = "route-root"
-              }
-              "type" = "ExtensionRef"
-            },
-          ]
-        },
-      ]
-    }
-  }
-}
+# resource "kubernetes_manifest" "http_route" {
+#   manifest = {
+#     "apiVersion" = "gateway.networking.k8s.io/v1beta1"
+#     "kind"       = "HTTPRoute"
+#     "metadata" = {
+#       "name"      = "route-root"
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "parentRefs" = [
+#         {
+#           "name"      = "api-gateway"
+#           "namespace" = "consul"
+#         },
+#       ]
+#       "rules" = [
+#         {
+#           "backendRefs" = [
+#             {
+#               "kind"      = "Service"
+#               "name"      = "store"
+#               "namespace" = var.namespace
+#               "port"      = local.service_ports.store
+#             },
+#           ]
+#           "matches" = [
+#             {
+#               "path" = {
+#                 "type"  = "PathPrefix"
+#                 "value" = "/"
+#               }
+#             },
+#           ]
+#           "filters" = [
+#             {
+#               "extensionRef" = {
+#                 "group" = "consul.hashicorp.com"
+#                 "kind"  = "RouteRetryFilter"
+#                 "name"  = "route-root"
+#               }
+#               "type" = "ExtensionRef"
+#             },
+#           ]
+#         },
+#       ]
+#     }
+#   }
+# }
 
 locals {
   customers_url = "http://customers.virtual.consul"
