9094c85617079d0bc7838c3844aaed0ab1b04c3a
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-26 15:36:39 -0500

    Revert "Remove sameness group"
    This reverts commit 13289f7f4f414234647a42a723ad26a709bc2ba4.

--- runtime_ec2/region/consul.tf
+++ runtime_ec2/region/consul.tf
@@ -60,7 +60,11 @@ resource "consul_config_entry" "exported_services_payments_ec2" {
     Services = [{
       Name      = "payments"
       Namespace = "default"
-      Consumers = concat(local.partitions)
+      Consumers = concat([
+        {
+          SamenessGroup = var.sameness_group
+        }
+      ], local.partitions)
     },{
       Name      = "mesh-gateway"
       Namespace = "default"
