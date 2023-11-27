51dd65f44a42ae0968204914477a53a1c46b0f65
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-27 13:46:11 -0800

    Remove inventory

--- runtime_eks/applications/region/inventory.tf
+++ runtime_eks/applications/region/inventory.tf
@@ -1,63 +1,63 @@
-module "inventory" {
-  source = "../modules/fake-service"
+# module "inventory" {
+#   source = "../modules/fake-service"
 
-  region        = var.region
-  name          = "inventory"
-  port          = local.service_ports.inventory
-  upstream_uris = ""
-  protocol      = "http"
-}
+#   region        = var.region
+#   name          = "inventory"
+#   port          = local.service_ports.inventory
+#   upstream_uris = ""
+#   protocol      = "http"
+# }
 
-module "inventory_v2" {
-  source = "../modules/fake-service"
+# module "inventory_v2" {
+#   source = "../modules/fake-service"
 
-  region        = var.region
-  name          = "inventory-v2"
-  port          = local.service_ports.inventory
-  upstream_uris = ""
-  protocol      = "http"
-}
+#   region        = var.region
+#   name          = "inventory-v2"
+#   port          = local.service_ports.inventory
+#   upstream_uris = ""
+#   protocol      = "http"
+# }
 
-resource "kubernetes_manifest" "service_intentions_inventory" {
-  manifest = {
-    "apiVersion" = "consul.hashicorp.com/v1alpha1"
-    "kind"       = "ServiceIntentions"
-    "metadata" = {
-      "name"      = "inventory"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "destination" = {
-        "name" = "inventory"
-      }
-      "sources" = [
-        {
-          "action" = "allow"
-          "name"   = "store"
-        }
-      ]
-    }
-  }
-}
+# resource "kubernetes_manifest" "service_intentions_inventory" {
+#   manifest = {
+#     "apiVersion" = "consul.hashicorp.com/v1alpha1"
+#     "kind"       = "ServiceIntentions"
+#     "metadata" = {
+#       "name"      = "inventory"
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "destination" = {
+#         "name" = "inventory"
+#       }
+#       "sources" = [
+#         {
+#           "action" = "allow"
+#           "name"   = "store"
+#         }
+#       ]
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "service_splitter_inventory" {
-  manifest = {
-    "apiVersion" = "consul.hashicorp.com/v1alpha1"
-    "kind"       = "ServiceSplitter"
-    "metadata" = {
-      "name"      = "inventory"
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "splits" = [
-        {
-          "weight" = var.enable_inventory_v2 ? 50 : 100
-        },
-        {
-          "service" = "inventory-v2"
-          "weight"  = var.enable_inventory_v2 ? 50 : 0
-        },
-      ]
-    }
-  }
-}
+# resource "kubernetes_manifest" "service_splitter_inventory" {
+#   manifest = {
+#     "apiVersion" = "consul.hashicorp.com/v1alpha1"
+#     "kind"       = "ServiceSplitter"
+#     "metadata" = {
+#       "name"      = "inventory"
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "splits" = [
+#         {
+#           "weight" = var.enable_inventory_v2 ? 50 : 100
+#         },
+#         {
+#           "service" = "inventory-v2"
+#           "weight"  = var.enable_inventory_v2 ? 50 : 0
+#         },
+#       ]
+#     }
+#   }
+# }
