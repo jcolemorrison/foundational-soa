4c77039a3a115f4a6c497e9d104e2b01bdd0b62c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-11-27 16:46:03 -0800

    Create everything but sameness group

--- runtime_eks/applications/region/sameness.tf
+++ runtime_eks/applications/region/sameness.tf
@@ -8,18 +8,18 @@ locals {
   }]
 }
 
-resource "kubernetes_manifest" "sameness_group" {
-  manifest = {
-    "apiVersion" = "consul.hashicorp.com/v1alpha1"
-    "kind"       = "SamenessGroup"
-    "metadata" = {
-      "name"      = var.sameness_group_name
-      "namespace" = var.namespace
-    }
-    "spec" = {
-      "includeLocal"       = false
-      "defaultForFailover" = true
-      "members"            = concat(local.partitions, local.peers)
-    }
-  }
-}
+# resource "kubernetes_manifest" "sameness_group" {
+#   manifest = {
+#     "apiVersion" = "consul.hashicorp.com/v1alpha1"
+#     "kind"       = "SamenessGroup"
+#     "metadata" = {
+#       "name"      = var.sameness_group_name
+#       "namespace" = var.namespace
+#     }
+#     "spec" = {
+#       "includeLocal"       = false
+#       "defaultForFailover" = true
+#       "members"            = concat(local.partitions, local.peers)
+#     }
+#   }
+# }
