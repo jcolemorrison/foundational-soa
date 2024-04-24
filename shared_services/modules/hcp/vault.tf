bd2dacd8376ba45fb7f7b1e4859dfee51101d852
J Cole Morrison <me@jcolemorrison.com>
2024-04-23 20:27:59 -0700

    reset vault token

--- shared_services/modules/hcp/vault.tf
+++ shared_services/modules/hcp/vault.tf
@@ -11,7 +11,6 @@ resource "hcp_vault_cluster" "vault" {
 }
 
 resource "hcp_vault_cluster_admin_token" "vault" {
-  # count      = var.hcp_vault_name != null ? 1 : 0
-  count = 0
+  count      = var.hcp_vault_name != null ? 1 : 0
   cluster_id = hcp_vault_cluster.vault.0.cluster_id
 }
