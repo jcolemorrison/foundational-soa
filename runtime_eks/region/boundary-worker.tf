7577431d478a976ecb69ab4f6edaa5faa2c2061e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-04-30 16:37:11 -0400

    Remove worker token

--- runtime_eks/region/boundary-worker.tf
+++ runtime_eks/region/boundary-worker.tf
@@ -1,32 +1,32 @@
 # Register worker into Boundary after its token is stored in Vault
 
-data "aws_instances" "boundary_worker" {
-  filter {
-    name   = "instance-id"
-    values = [module.boundary_worker.0.instance_id]
-  }
-  instance_state_names = ["running"]
-}
+# data "aws_instances" "boundary_worker" {
+#   filter {
+#     name   = "instance-id"
+#     values = [module.boundary_worker.0.instance_id]
+#   }
+#   instance_state_names = ["running"]
+# }
 
-data "vault_kv_secret_v2" "boundary_worker_token_eks" {
-  count     = length(data.aws_instances.boundary_worker) > 0 ? 1 : 0
-  mount     = var.boundary_worker_vault_path
-  namespace = var.boundary_worker_vault_namespace
-  name      = "${var.region}-${var.runtime}-${split(".", module.boundary_worker.0.private_dns).0}"
-}
+# data "vault_kv_secret_v2" "boundary_worker_token_eks" {
+#   count     = length(data.aws_instances.boundary_worker) > 0 ? 1 : 0
+#   mount     = var.boundary_worker_vault_path
+#   namespace = var.boundary_worker_vault_namespace
+#   name      = "${var.region}-${var.runtime}-${split(".", module.boundary_worker.0.private_dns).0}"
+# }
 
-resource "boundary_worker" "eks" {
-  count                       = length(data.aws_instances.boundary_worker) > 0 ? 1 : 0
-  depends_on                  = [module.boundary_worker, data.vault_kv_secret_v2.boundary_worker_token_eks]
-  scope_id                    = "global"
-  name                        = data.vault_kv_secret_v2.boundary_worker_token_eks.0.name
-  description                 = "Self-managed worker ${data.vault_kv_secret_v2.boundary_worker_token_eks.0.name} for EKS"
-  worker_generated_auth_token = data.vault_kv_secret_v2.boundary_worker_token_eks.0.data.token
+# resource "boundary_worker" "eks" {
+#   count                       = length(data.aws_instances.boundary_worker) > 0 ? 1 : 0
+#   depends_on                  = [module.boundary_worker, data.vault_kv_secret_v2.boundary_worker_token_eks]
+#   scope_id                    = "global"
+#   name                        = data.vault_kv_secret_v2.boundary_worker_token_eks.0.name
+#   description                 = "Self-managed worker ${data.vault_kv_secret_v2.boundary_worker_token_eks.0.name} for EKS"
+#   worker_generated_auth_token = data.vault_kv_secret_v2.boundary_worker_token_eks.0.data.token
 
-  lifecycle {
-    precondition {
-      condition     = length(data.vault_kv_secret_v2.boundary_worker_token_eks) > 0
-      error_message = "The Boundary EC2 instance has not registered its worker auth token into Vault yet. Wait and re-apply Terraform to register worker."
-    }
-  }
-}
+#   lifecycle {
+#     precondition {
+#       condition     = length(data.vault_kv_secret_v2.boundary_worker_token_eks) > 0
+#       error_message = "The Boundary EC2 instance has not registered its worker auth token into Vault yet. Wait and re-apply Terraform to register worker."
+#     }
+#   }
+# }
