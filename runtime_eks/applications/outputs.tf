546e980bd8eaf9e8ce934ba211d9102c3206b602
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-04-30 16:23:45 -0400

    Remove lb

--- runtime_eks/applications/outputs.tf
+++ runtime_eks/applications/outputs.tf
@@ -1,41 +1,41 @@
-data "aws_lb" "lb_us_east_1" {
-  tags = {
-    "elbv2.k8s.aws/cluster" = local.cluster_name
-    "service.k8s.aws/stack" = "${var.namespace}/store"
-  }
-  provider = aws
-}
+# data "aws_lb" "lb_us_east_1" {
+#   tags = {
+#     "elbv2.k8s.aws/cluster" = local.cluster_name
+#     "service.k8s.aws/stack" = "${var.namespace}/store"
+#   }
+#   provider = aws
+# }
 
-data "aws_lb" "lb_us_west_2" {
-  tags = {
-    "elbv2.k8s.aws/cluster" = local.cluster_name
-    "service.k8s.aws/stack" = "${var.namespace}/store"
-  }
-  provider = aws.us_west_2
-}
+# data "aws_lb" "lb_us_west_2" {
+#   tags = {
+#     "elbv2.k8s.aws/cluster" = local.cluster_name
+#     "service.k8s.aws/stack" = "${var.namespace}/store"
+#   }
+#   provider = aws.us_west_2
+# }
 
-data "aws_lb" "lb_eu_west_1" {
-  tags = {
-    "elbv2.k8s.aws/cluster" = local.cluster_name
-    "service.k8s.aws/stack" = "${var.namespace}/store"
-  }
-  provider = aws.eu_west_1
-}
+# data "aws_lb" "lb_eu_west_1" {
+#   tags = {
+#     "elbv2.k8s.aws/cluster" = local.cluster_name
+#     "service.k8s.aws/stack" = "${var.namespace}/store"
+#   }
+#   provider = aws.eu_west_1
+# }
 
-output "public_alb_dns_values" {
-  value = {
-    "us-east-1" = {
-      dns_name = data.aws_lb.lb_us_east_1.dns_name
-      zone_id  = data.aws_lb.lb_us_east_1.zone_id
-    }
-    "us-west-2" = {
-      dns_name = data.aws_lb.lb_us_west_2.dns_name
-      zone_id  = data.aws_lb.lb_us_west_2.zone_id
-    }
-    "eu-west-1" = {
-      dns_name = data.aws_lb.lb_eu_west_1.dns_name
-      zone_id  = data.aws_lb.lb_eu_west_1.zone_id
-    }
-  }
-  description = "DNS Name and Zone ID of store service per region"
-}
+# output "public_alb_dns_values" {
+#   value = {
+#     "us-east-1" = {
+#       dns_name = data.aws_lb.lb_us_east_1.dns_name
+#       zone_id  = data.aws_lb.lb_us_east_1.zone_id
+#     }
+#     "us-west-2" = {
+#       dns_name = data.aws_lb.lb_us_west_2.dns_name
+#       zone_id  = data.aws_lb.lb_us_west_2.zone_id
+#     }
+#     "eu-west-1" = {
+#       dns_name = data.aws_lb.lb_eu_west_1.dns_name
+#       zone_id  = data.aws_lb.lb_eu_west_1.zone_id
+#     }
+#   }
+#   description = "DNS Name and Zone ID of store service per region"
+# }
