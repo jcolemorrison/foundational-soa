b8b8542f7471794f6e1cb82c8a4eb4ff77c939b9
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-05-01 12:08:35 -0400

    Revert changes for deletion

--- runtime_eks/database-access.tf
+++ runtime_eks/database-access.tf
@@ -11,8 +11,7 @@ module "database_vault" {
 
   db_name     = module.us_east_1.database.dbname
   db_username = module.us_east_1.database.username
-  # db_password = module.us_east_1.database.password
-  db_password = null
+  db_password = module.us_east_1.database.password
   db_address  = module.us_east_1.database.address
   db_port     = module.us_east_1.database.port
 
