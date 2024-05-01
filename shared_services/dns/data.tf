7734eb4505166306640d3b03dcbcc61706a0aaf5
J Cole Morrison <me@jcolemorrison.com>
2024-05-01 14:19:03 -0700

    account for lack of shared workspaces

--- shared_services/dns/data.tf
+++ shared_services/dns/data.tf
@@ -44,6 +44,6 @@ data "terraform_remote_state" "runtime_eks" {
 
 
 locals {
-  aws_default_region = data.terraform_remote_state.shared_services.outputs.default_region.id
-  aws_default_tags   = data.terraform_remote_state.shared_services.outputs.default_tags
+  aws_default_region = var.aws_default_region != null ? var.aws_default_region : data.terraform_remote_state.shared_services.outputs.default_region.id
+  aws_default_tags   = var.aws_default_tags != null ? var.aws_default_tags : data.terraform_remote_state.shared_services.outputs.default_tags
 }
--- shared_services/dns/variables.tf
+++ shared_services/dns/variables.tf
@@ -7,3 +7,17 @@ variable "domain_name" {
   type        = string
   description = "apex domain name of all services. i.e. hashidemo.com"
 }
+
+variable "aws_default_region" {
+  type        = string
+  description = "The default region that all resources will be deployed into."
+  default     = "us-east-1"
+}
+
+variable "aws_default_tags" {
+  type        = map(string)
+  description = "Default tags added to all AWS resources."
+  default = {
+    Project = "foundational-soa-shared-services"
+  }
+}
